0e2e90eaffdf71a2a8cba6168a31089f69ab39cb
dedekkjx-boop <dedekkjx@gmail.com>
2026-09-20 11:26:51 -0400

    Update script loading URLs in script.js

--- api/script.js
+++ api/script.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
   }
 
   const script = `
-loadstring(game:HttpGet'https://pastefy.app/JyYjz6qQ/raw')()
+loadstring(game:HttpGet'https://pastefy.app/snwHueyt/raw')()
 `;
 
   res.setHeader("Content-Type", "text/plain");
